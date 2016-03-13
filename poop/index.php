fefd3a576b2dea2382b2332d0c196b7971a83ae6
Glaser-Allie <gla13006@byui.edu>
2016-03-13 06:24:12 -0500

    pretty but not functional html/css
    This folder contains html/css code based on the proposed wireframes but does not integrate the functionality of the cards/flip or json files.
    live demo at allieglaser.com/poop

---
+++ poop/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/poop/modules/head.php'; ?>
+    </head>
+    
+    <body>
+        <section class="wrapper">
+            <header>
+                <?php include $_SERVER['DOCUMENT_ROOT'].'/poop/modules/header.php'; ?> 
+            </header>
+
+            <section id="main">
+                <section class="animated shake">
+                    <img src="/poop/images/poop_logo.png" id="poop_logo">
+                </section>
+                <section class="page_title"><h2><a href="/poop/index.php">FUN WITH POOP</a><h2>
+                </section>
+            </section>
+
+            <footer>
+                <?php include $_SERVER['DOCUMENT_ROOT'].'/poop/modules/footer.php'; ?>
+            </footer>
+        </section>
+    </body>
+</html>
---
+++ poop/modules/footer.php
@@ -0,0 +1,4 @@
+<ul id="footer-links">
+    <!--<li><a href="/poop/index.php">HOME</a></li> -->
+    <!--<li></li>-->
+</ul> 
