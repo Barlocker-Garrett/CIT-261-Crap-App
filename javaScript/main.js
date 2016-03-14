0b3c1d5e7aae471ab844a4fb856f4d208b90f36b
Landon-Whitney <whitney.landon@gmail.com>
2016-03-14 10:50:11 -0500

    Lasting Click Effect

--- javaScript/main.js
+++ javaScript/main.js
@@ -78,3 +78,19 @@ function changeToJokes() {
     document.getElementById('Topic').title = 'Jokes';
     loadObjectsFromLocal(true);
 }
+
+//show card has been flipped
+
+var cards = document.querySelectorAll(".card.effect__click");
+var len;
+for ( var i = 0, len = cards.length; i < len; i++) {
+	var card = cards[i];
+	clickListener(card);
+}
+	
+function clickListener(card) {
+	card.addEventListener("click", function() {
+		var c = this.classList;
+		c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+	});
+}
