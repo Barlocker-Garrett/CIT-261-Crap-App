4bc3e49c97b53c2f38ec30e63825a76b19631f71
Garrett <Garrett@Tech-Garrett>
2016-03-19 23:43:39 -0700

    Added persistence to viewed Jokes and Facts By adding saves to localStorage "Jokes" and "Facts" Need to still separate out into functions to make it better and optimize code.

--- poop/javaScript/main.js
+++ poop/javaScript/main.js
@@ -2,7 +2,7 @@ var httpRequest;
 
 // This NumContent object is used for the splicing, this acts "like" an ENUM
 var NumContent;
-              
+
 function loadContent() {
     makeRequest('content.json');
 }
@@ -23,7 +23,9 @@ function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
             Content = JSON.parse(httpRequest.responseText);
-            if (localStorage.getItem("Content") !== null) {
+            if (localStorage.getItem("Content") !== null ||
+                    localStorage.getItem("Jokes") !== null ||
+                    localStorage.getItem("Facts") !== null) {
                 loadObjectsFromLocal();
             }
         }
@@ -34,20 +36,39 @@ function alertContents() {
     }
 }
 
-function loadObjectsFromLocal(reset) {
-    NumContent = Object.freeze({"total":JSON.parse(localStorage.Content).length, 
-                                "jokes":9, 
-                                "facts":20});
-                            
-    if (Content.length < 1 || reset === true) {
-        Content.splice(0,NumContent.total);
+function loadObjectsFromLocal() {
+    NumContent = Object.freeze({"total": JSON.parse(localStorage.Content).length,
+        "jokes": 9,
+        "facts": 20});
+    
+    if (Content.length < 1) {
+        Content.splice(0, NumContent.total);
         Content = JSON.parse(localStorage.Content);
     }
-    if (document.getElementById('Topic').title === 'Jokes') {
+    if (localStorage.getItem("Jokes") !== null && document.getElementById('Topic').title === 'Jokes'){
+            if (Content.length === (NumContent.total))
+            {
+                Content.splice(NumContent.jokes, NumContent.total);
+                localStorage.setItem("Jokes", JSON.stringify(Content));
+            }
+        Content = JSON.parse(localStorage.Jokes);
+    }
+    else if (document.getElementById('Topic').title === 'Jokes') {
         Content.splice(NumContent.jokes, NumContent.total);
-    // THE 29 IN THE ELSE IF IS THE TOTAL NUMBER OF JOKES AND FACTS
-    } else if (document.getElementById('Topic').title === 'Facts' && Content.length === NumContent.total) {
+        localStorage.setItem("Jokes", JSON.stringify(Content));
+    } 
+    
+    if (localStorage.getItem("Facts") !== null && document.getElementById('Topic').title === 'Facts'){
+            if (Content.length === (NumContent.total))
+            {
+                Content.splice(0, NumContent.jokes);
+                localStorage.setItem("Facts", JSON.stringify(Content));
+            }
+        Content = JSON.parse(localStorage.Facts);
+    }
+    if (document.getElementById('Topic').title === 'Facts' && Content.length === NumContent.total) {
         Content.splice(0, NumContent.jokes);
+        localStorage.setItem("Facts", JSON.stringify(Content));
     }
     selectContent();
 }
@@ -56,16 +77,23 @@ function selectContent() {
     var index;
 
     index = Math.floor(Math.random() * Content.length);
-    
-    if(document.getElementById('Topic').title === 'Jokes') {
+
+    if (document.getElementById('Topic').title === 'Jokes') {
         document.getElementById("text").innerHTML = Content[index].joke;
         document.getElementById("pun").innerHTML = Content[index].pun;
-    }
-    else if (document.getElementById('Topic').title === 'Facts') {
+    } else if (document.getElementById('Topic').title === 'Facts') {
         document.getElementById("text").innerHTML = Content[index].fact;
     }
-    
+
     Content.splice(index, 1);
+    if (document.getElementById('Topic').title === 'Jokes') {
+        localStorage.removeItem("Jokes");
+        localStorage.setItem("Jokes", JSON.stringify(Content));
+    } else if (document.getElementById('Topic').title === 'Facts') {
+        localStorage.removeItem("Facts");
+        localStorage.setItem("Facts", JSON.stringify(Content));
+    }
+
     console.log(Content); //THIS SHOWS IN THE CONSOLE THAT IT WORKS
 }
 
@@ -83,20 +111,19 @@ function changeToJokes() {
 
 var cards = document.querySelectorAll(".card.effect__click");
 var len;
-for ( var i = 0, len = cards.length; i < len; i++) {
-	var card = cards[i];
-	clickListener(card);
+for (var i = 0, len = cards.length; i < len; i++) {
+    var card = cards[i];
+    clickListener(card);
 }
-	
+
 function clickListener(card) {
-	card.addEventListener("click", function() {
-		var c = this.classList;
-		if (c.contains("flipped") === true) {
-			c.remove("flipped");
-			loadObjectsFromLocal();
-		}
-		else {
-			 c.add("flipped");
-		}
-	});
+    card.addEventListener("click", function () {
+        var c = this.classList;
+        if (c.contains("flipped") === true) {
+            c.remove("flipped");
+            loadObjectsFromLocal();
+        } else {
+            c.add("flipped");
+        }
+    });
 }
