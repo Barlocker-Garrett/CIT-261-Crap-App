5175716096387831616397e0524084190a04456e
Glaser-Allie <gla13006@byui.edu>
2016-03-21 21:48:24 -0500

    This version seems to work best
    This version of Whitney's js file seems to be the most functional

--- poop/javaScript/main.js
+++ poop/javaScript/main.js
@@ -2,7 +2,8 @@ var httpRequest;
 
 // This NumContent object is used for the splicing, this acts "like" an ENUM
 var NumContent;
-
+var Content = [];
+              
 function loadContent() {
     makeRequest('content.json');
 }
@@ -14,8 +15,8 @@ function makeRequest(url) {
         alert('Giving up :( Cannot create an XMLHTTP instance');
         return false;
     }
+    httpRequest.onreadystatechange = alertContents();
     httpRequest.open('GET', url);
-    httpRequest.onreadystatechange = alertContents;
     httpRequest.send();
 }
 
@@ -23,9 +24,7 @@ function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
         if (httpRequest.status === 200) {
             Content = JSON.parse(httpRequest.responseText);
-            if (localStorage.getItem("Content") !== null ||
-                    localStorage.getItem("Jokes") !== null ||
-                    localStorage.getItem("Facts") !== null) {
+            if (localStorage.getItem("Content") !== null) {
                 loadObjectsFromLocal();
             }
         }
@@ -36,50 +35,20 @@ function alertContents() {
     }
 }
 
-function loadObjectsFromLocal() {
-    NumContent = Object.freeze({"total": JSON.parse(localStorage.Content).length,
-        "jokes": 16,
-        "facts": 38});
-    
-    // IF WE NEED TO RESET THE CONTENT
-    if (Content.length < 1) {  
-        Content.splice(0, NumContent.total);
+function loadObjectsFromLocal(reset) {
+    NumContent = Object.freeze({"total":JSON.parse(localStorage.Content).length, 
+                                "jokes":16, 
+                                "facts":20});
+                            
+    if (Content.length < 1 || reset === true) {
+        Content.splice(0,NumContent.total);
         Content = JSON.parse(localStorage.Content);
     }
-    
-    // IF THE CONTENT NEED TO BE LOADED FROM JOKES KEY
-    if (localStorage.getItem("Jokes") !== null && document.getElementById('Topic').title === 'Jokes'){
-            if (Content.length === (NumContent.total))
-            {
-                Content.splice(NumContent.jokes, NumContent.total);
-                var Jokes = JSON.parse(localStorage.Jokes);
-                // IF WE NEED TO RESET DON'T SAVE JOKES KEY INTO CONTENT
-                if (Jokes.length !== 0)
-                {
-                    Content = JSON.parse(localStorage.Jokes);
-                }
-                localStorage.setItem("Jokes", JSON.stringify(Content));
-            }
-        Content = JSON.parse(localStorage.Jokes);
-    } // THE JOKES KEY NEEDS TO SPLICED INTO LOCAL STORAGE FROM CONTENT KEY
-    else if (document.getElementById('Topic').title === 'Jokes') {
+    if (document.getElementById('Topic').title === 'Jokes') {
         Content.splice(NumContent.jokes, NumContent.total);
-        localStorage.setItem("Jokes", JSON.stringify(Content));
-    } 
-    
-    // IF THE CONTENT NEED TO BE LOADED FROM FACTS KEY
-    if (localStorage.getItem("Facts") !== null && document.getElementById('Topic').title === 'Facts'){
-            if (Content.length === (NumContent.total))
-            {
-                Content.splice(0, NumContent.jokes);
-                localStorage.setItem("Facts", JSON.stringify(Content));
-            }
-        Content = JSON.parse(localStorage.Facts);
-    } // THE FACTS KEY NEEDS TO SPLICED INTO LOCAL STORAGE FROM CONTENT KEY
-    else if (document.getElementById('Topic').title === 'Facts') {
-        Content = JSON.parse(localStorage.Content);
+    // THE 29 IN THE ELSE IF IS THE TOTAL NUMBER OF JOKES AND FACTS
+    } else if (document.getElementById('Topic').title === 'Facts' && Content.length === NumContent.total) {
         Content.splice(0, NumContent.jokes);
-        localStorage.setItem("Facts", JSON.stringify(Content));
     }
     selectContent();
 }
@@ -88,33 +57,38 @@ function selectContent() {
     var index;
 
     index = Math.floor(Math.random() * Content.length);
-
-    if (document.getElementById('Topic').title === 'Jokes') {
+    
+    if(document.getElementById('Topic').title === 'Jokes') {
         document.getElementById("text").innerHTML = Content[index].joke;
         document.getElementById("pun").innerHTML = Content[index].pun;
-    } else if (document.getElementById('Topic').title === 'Facts') {
+    }
+    else if (document.getElementById('Topic').title === 'Facts') {
         document.getElementById("text").innerHTML = Content[index].fact;
+        document.getElementById("pun").innerHTML = "";
     }
-
+    
     Content.splice(index, 1);
-    if (document.getElementById('Topic').title === 'Jokes') {
-        localStorage.removeItem("Jokes");
-        localStorage.setItem("Jokes", JSON.stringify(Content));
-    } else if (document.getElementById('Topic').title === 'Facts') {
-        localStorage.removeItem("Facts");
-        localStorage.setItem("Facts", JSON.stringify(Content));
-    }
-
     console.log(Content); //THIS SHOWS IN THE CONSOLE THAT IT WORKS
+}
+
+function changeToMain () {
+	document.getElementById('card').style.display='none';
+	document.getElementById('home').style.display='initial';
 }
 
 function changeToFacts() {
     document.getElementById('Topic').title = 'Facts';
+    document.getElementById('title').innerHTML = "POOP FACTS";
+    document.getElementById('card').style.display = "initial";
+    document.getElementById('home').style.display='none';
     loadObjectsFromLocal(true);
 }
 
 function changeToJokes() {
     document.getElementById('Topic').title = 'Jokes';
+    document.getElementById('title').innerHTML = "POOP JOKES";
+    document.getElementById('card').style.display = "initial";
+    document.getElementById('home').style.display='none';
     loadObjectsFromLocal(true);
 }
 
@@ -122,19 +96,20 @@ function changeToJokes() {
 
 var cards = document.querySelectorAll(".card.effect__click");
 var len;
-for (var i = 0, len = cards.length; i < len; i++) {
-    var card = cards[i];
-    clickListener(card);
+for ( var i = 0, len = cards.length; i < len; i++) {
+	var card = cards[i];
+	clickListener(card);
 }
-
+	
 function clickListener(card) {
-    card.addEventListener("click", function () {
-        var c = this.classList;
-        if (c.contains("flipped") === true) {
-            c.remove("flipped");
-            loadObjectsFromLocal();
-        } else {
-            c.add("flipped");
-        }
-    });
+	card.addEventListener("click", function() {
+		var c = this.classList;
+		if (c.contains("flipped") === true) {
+			c.remove("flipped");
+			loadObjectsFromLocal();
+		}
+		else {
+			 c.add("flipped");
+		}
+	});
 }
