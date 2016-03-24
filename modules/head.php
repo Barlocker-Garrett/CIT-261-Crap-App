626efcb5baa6efc86bd2a898eed434a58cf41827
Glaser-Allie <gla13006@byui.edu>
2016-03-23 21:39:43 -0500

    iOS & Android mobile web app capacity & icon

--- modules/head.php
+++ modules/head.php
@@ -2,6 +2,12 @@
 <title>FunWithPoop</title>
 <meta name="author" content="Team 7">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- iOS -->
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<!--Android-->
+<link rel="icon" sizes="192x192" href="nice-highres.png"> 
+<meta name="mobile-web-app-capable" content="yes">
 <!-- css -->
 <link href='https://fonts.googleapis.com/css?family=Luckiest+Guy|Josefin+Sans:400,300' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="/css/stylesheet.css"  media="screen">
+<link rel="stylesheet" type="text/css" href="/poop/css/stylesheet.css"  media="screen">
