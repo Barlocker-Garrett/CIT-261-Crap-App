bf3f3c687d55b551e8e84b3fd7ddbdca44ed9d30
Sierra Pack <sierrajpack@gmail.com>
2016-03-25 14:10:43 -0600

    3/25/16 Stylesheet and Index Updated to Implement CSS Responsive Page Change

--- index.php
+++ index.php
@@ -5,37 +5,74 @@
     </head>
     
     <body ontouchstart="" onload="loadContent()">
-        <section class="wrapper">
-            <header>
-                <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?> 
-            </header>
 
-            <section id="main">
-            	<section id="home">
-                	<section class="animated shake">
-                    	<img src="/images/poop_logo.png" id="poop_logo">
-                	</section>
-                	<section class="page_title">
-                		<h2><a href="/index.php">FUN WITH POOP</a><h2>
-                	</section>
-                </section>
-                <section id="card">
-                <div class="container">
-                    <div id="Topic" title="Jokes" class="page_title"><h2 id="title">POOP JOKES</h2>
-                    </div>
-                    <div class="container">
-                        <div class="card effect__click">
-                            <div class="card__front">
-                                <span class="card__text" id="text"></span>
+        <div class="container">
+			
+		<div class="st-container">
+			
+                <input type="radio" name="radio-set" checked="checked" id="st-control-1"/>
+			<a href="#st-panel-1">Fun Poop</a>
+		<input type="radio" name="radio-set" id="st-control-2"/>
+			<a href="#st-panel-2" onclick="changeToJokes()">Poop Jokes</a>
+                <input type="radio" name="radio-set" id="st-control-3"/>
+			<a href="#st-panel-3" onclick="changeToFacts()">Poop Facts</a>
+				
+                    <div class="st-scroll">
+					
+			<section class="st-panel" id="st-panel-1">
+			<div class="st-deco" data-icon="O"></div>
+                            <h2>Fun With Poop</h2>
+			<p>
+                            <img id="logo"  src="/images/poop_logo.png">
+                        </p>
+			</section>
+					
+			<section class="st-panel st-color" id="st-panel-2">
+			<div class="st-deco" data-icon="R"></div>
+				<h2>Poop Jokes</h2>
+						
+                            <div id="card">
+                            <div class="card-container">  
+                                
+                                <div class="card effect__click">
+                                    <div class="card__front">
+                                        <span class="card__text" id="text"></span>
+                                    </div>
+                                    <div class="card__back">
+                                        <span class="card__text" id="pun"></span>
+                                    </div>
+                                </div>
+                                
                             </div>
-                            <div class="card__back">
-                                <span class="card__text" id="pun"></span>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            	</section>
-            </section>
+                        </section>
+					   
+                    
+                        <section class="st-panel" id="st-panel-3">
+			<div class="st-deco" data-icon="Q"></div>
+				<h2>Poop Facts</h2>
+						
+                        <div id="card">
+                            <div class="card-container">  
+                                
+                                <div class="card effect__click">
+                                    <div class="card__front">
+                                        <span class="card__text" id="text"></span>
+                                    </div>
+                                    <div class="card__back">
+                                        <span class="card__text" id="pun"></span>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            </div>
+			</section>
+
+			</div><!-- // st-scroll -->
+				
+		</div><!-- // st-container -->
+			
+            </div><!-- // container -->
             <footer>
                 <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>
             </footer>
